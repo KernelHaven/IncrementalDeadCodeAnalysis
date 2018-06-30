95713502ad2fd1f2914fb7c1b74539bce71c8ef1
moritz <moritzf@gmail.com>
2018-06-30 09:46:40 +0200

    Fix checkstyle

--- src/net/ssehub/kernel_haven/incremental/analysis/IncrementalThreadedDeadCodeAnalysis.java
+++ src/net/ssehub/kernel_haven/incremental/analysis/IncrementalThreadedDeadCodeAnalysis.java
@@ -6,65 +6,76 @@ import net.ssehub.kernel_haven.analysis.PipelineAnalysis;
 import net.ssehub.kernel_haven.config.Configuration;
 import net.ssehub.kernel_haven.incremental.settings.IncrementalAnalysisSettings;
 import net.ssehub.kernel_haven.incremental.storage.HybridCacheAdapter;
-import net.ssehub.kernel_haven.incremental.storage.IncrementalPostExtraction;
 import net.ssehub.kernel_haven.incremental.storage.HybridCacheAdapter.CodeModelProcessing;
-import net.ssehub.kernel_haven.undead_analyzer.DeadCodeAnalysis;
+import net.ssehub.kernel_haven.incremental.storage.IncrementalPostExtraction;
 import net.ssehub.kernel_haven.undead_analyzer.ThreadedDeadCodeFinder;
 import net.ssehub.kernel_haven.util.Logger;
 
+// TODO: Auto-generated Javadoc
 /**
- * Incremental implementation of the {@link DeadCodeAnalysis}
+ * Incremental implementation of the {@link DeadCodeAnalysis}.
  *
  * @author Moritz
  */
 public class IncrementalThreadedDeadCodeAnalysis extends PipelineAnalysis {
 
-	Logger LOGGER = Logger.get();
+    /** The Constant LOGGER. */
+    private static final Logger LOGGER = Logger.get();
 
-	/**
-	 * Instantiates a new incremental dead code analysis.
-	 *
-	 * @param config
-	 *            the config
-	 */
-	public IncrementalThreadedDeadCodeAnalysis(Configuration config) {
-		super(config);
-	}
+    /**
+     * Instantiates a new incremental dead code analysis.
+     *
+     * @param config
+     *            the config
+     */
+    public IncrementalThreadedDeadCodeAnalysis(Configuration config) {
+        super(config);
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see net.ssehub.kernel_haven.analysis.PipelineAnalysis#createPipeline()
-	 */
-	@Override
-	protected AnalysisComponent<?> createPipeline() throws SetUpException {
-		boolean updatedBuildModel = config.getValue(IncrementalAnalysisSettings.EXTRACT_BUILD_MODEL);
-		boolean updatedVariabilityModel = config.getValue(IncrementalAnalysisSettings.EXTRACT_VARIABILITY_MODEL);
+    /**
+     * Creates the pipeline.
+     *
+     * @return the analysis component
+     * @throws SetUpException the set up exception
+     */
+    /*
+     * (non-Javadoc)
+     * 
+     * @see net.ssehub.kernel_haven.analysis.PipelineAnalysis#createPipeline()
+     */
+    @Override
+    protected AnalysisComponent<?> createPipeline() throws SetUpException {
+        boolean updatedBuildModel =
+            config.getValue(IncrementalAnalysisSettings.EXTRACT_BUILD_MODEL);
+        boolean updatedVariabilityModel = config
+            .getValue(IncrementalAnalysisSettings.EXTRACT_VARIABILITY_MODEL);
 
-		CodeModelProcessing cmProcessing;
+        CodeModelProcessing cmProcessing;
 
-		if (!updatedBuildModel && !updatedVariabilityModel) {
-			cmProcessing = CodeModelProcessing.PARTIAL;
-			LOGGER.logInfo(
-					"Neither build nor variability-model were modified compared to the last revision that was analyzed. Therefore "
-							+ IncrementalDeadCodeAnalysis.class.getSimpleName()
-							+ " will run on the updated parts of the code-model only.");
-		} else {
-			cmProcessing = CodeModelProcessing.COMPLETE;
-			LOGGER.logInfo(
-					"variability and/or build-model was modified compared to the last revision that was analyzed. Therefore "
-							+ IncrementalDeadCodeAnalysis.class.getSimpleName() + " will perform a full analysis.");
-		}
+        if (!updatedBuildModel && !updatedVariabilityModel) {
+            cmProcessing = CodeModelProcessing.PARTIAL;
+            LOGGER.logInfo("Neither build nor variability-model were modified "
+                + "compared to the last revision that was analyzed. Therefore "
+                + IncrementalDeadCodeAnalysis.class.getSimpleName()
+                + " will run on the updated parts of the code-model only.");
+        } else {
+            cmProcessing = CodeModelProcessing.COMPLETE;
+            LOGGER.logInfo("variability and/or build-model was modified "
+                + "compared to the last revision that was analyzed. Therefore "
+                + IncrementalDeadCodeAnalysis.class.getSimpleName()
+                + " will perform a full analysis.");
+        }
 
-		HybridCacheAdapter hca = new HybridCacheAdapter(config,
-				new IncrementalPostExtraction(config, getCmComponent(), getBmComponent(), getVmComponent()),
-				cmProcessing);
+        HybridCacheAdapter hca = new HybridCacheAdapter(config,
+            new IncrementalPostExtraction(config, getCmComponent(),
+                getBmComponent(), getVmComponent()),
+            cmProcessing);
 
-		ThreadedDeadCodeFinder dcf = new ThreadedDeadCodeFinder(config, hca.getVmComponent(), hca.getBmComponent(),
-				hca.getCmComponent());
+        ThreadedDeadCodeFinder dcf = new ThreadedDeadCodeFinder(config,
+            hca.getVmComponent(), hca.getBmComponent(), hca.getCmComponent());
 
-		return dcf;
+        return dcf;
 
-	}
+    }
 
 }
