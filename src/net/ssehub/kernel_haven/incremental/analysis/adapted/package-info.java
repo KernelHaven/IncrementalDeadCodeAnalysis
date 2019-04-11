77540b132fecc0257e26fc02b45c3f4f4d6827e9
moritz <moritzf@gmail.com>
2019-04-11 10:41:52 +0200

    Remove adapted version of the dead code analysis

--- src/net/ssehub/kernel_haven/incremental/analysis/adapted/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- * This package contains incremental analysis classes that were adapted
- * using the {@link net.ssehub.kernel_haven.incremental.storage.HybridCacheAdapter}.
- * 
- * @author moritz
- *
- */
-package net.ssehub.kernel_haven.incremental.analysis.adapted;
